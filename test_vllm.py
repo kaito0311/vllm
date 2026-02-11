2ce168b6d7c1172cf1e97df3b604a517c63c3dd6
kaito0311 <tanminh8b@gmail.com>
2026-02-11 15:52:28 +0700

    feat: adding processor class

--- test_vllm.py
+++ test_vllm.py
@@ -2,7 +2,7 @@ from vllm import LLM, SamplingParams
 from PIL import Image 
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-def main():
+def test_vllm_generate():
     # llm = LLM("./pretrained_models/SmolVLM-256M-Instruct", kv_cache_memory_bytes=0, cpu_offload_gb=8, max_num_seqs=1, max_model_len=4096)
     llm = LLM("HuggingFaceTB/SmolVLM-256M-Instruct", kv_cache_memory_bytes=0, cpu_offload_gb=8, max_num_seqs=1, max_model_len=4096)
     llm._cached_repr = "<vLLM.LLM Object - Debug Mode>"
@@ -23,7 +23,49 @@ def main():
     for o in outputs:
         generated_text = o.outputs[0].text
         print("Generated Text:\n", generated_text)
+
+def test_vllm_chat():
+
+    llm = LLM("HuggingFaceTB/SmolVLM-256M-Instruct", kv_cache_memory_bytes=0, cpu_offload_gb=8, max_num_seqs=1, max_model_len=4096, gpu_memory_utilization=0.7)
+    llm._cached_repr = "<vLLM.LLM Object - Debug Mode>"
+
+    image = Image.open("images/test_image.jpg").convert("RGB")
+
+    conversation = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+        {"role": "assistant", "content": "Hello! How can I assist you today?"},
+        {
+            "role": "user",
+            "content": [
+           
+                {
+                    "type": "image_pil",
+                    "image_pil": image,
+                },
+           
+                {
+                    "type": "text",
+                    "text": "What's in these images?",
+                },
+            ],
+        },
+    ]
+
+    # Perform inference and log output.
+    outputs = llm.chat(conversation)
+
+    for o in outputs:
+        generated_text = o.outputs[0].text
+        print(generated_text)
+
+def main():
+
+    # print("Testing vLLM chat...")
+    # test_vllm_chat()
     
+    print("Testing vLLM generate...")
+    test_vllm_generate()
 
 
 if __name__ == "__main__":
--- vllm/model_executor/models/nano_vlm.py
+++ vllm/model_executor/models/nano_vlm.py
@@ -1,9 +1,15 @@
 
-from typing import cast, Annotated, Literal, TypeAlias
+from typing import Optional, cast, Annotated, Literal, TypeAlias, Union
+from itertools import accumulate
 
-import torch 
+import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
 from transformers import ProcessorMixin
+from transformers.tokenization_utils_base import BatchEncoding
+from transformers.feature_extraction_utils import BatchFeature
+from transformers.image_processing_base import BatchFeature as ImageBatchFeature
 
 from vllm.config import VllmConfig
 from vllm.transformers_utils.configs.nano_vlm import NanoVLMConfig
@@ -18,6 +24,7 @@ from .interfaces import MultiModalEmbeddings
 from .nano_vlm_modules.language_model import LanguageModel
 from .nano_vlm_modules.vision_transformer import ViT
 from .nano_vlm_modules.modality_projector import ModalityProjector
+from .nano_vlm_modules.utils.custom_transforms import DynamicResize, GlobalAndSplitImages
 
 
 class NanoVLMImagePixelInputs(TensorSchema):
@@ -47,20 +54,242 @@ class NanoVLMImageEmbeddingInputs(TensorSchema):
     type: Literal["image_embeds"]
     data: Annotated[torch.Tensor, TensorShape("bn", "f", "h")]
 
+
 ImageInputs: TypeAlias = NanoVLMImagePixelInputs | NanoVLMImageEmbeddingInputs
+
+
+class NanoVLMImageProcessor:
+    def __init__(self, max_image_size: int, patch_size: int, resize_to_max_size_len: bool = False):
+        self.max_image_size = max_image_size
+        self.patch_size = patch_size
+        self.resize_to_max_size_len = resize_to_max_size_len
+
+        self.transforms = transforms.Compose([
+            DynamicResize(
+                patch_size=self.patch_size,
+                max_side_len=self.max_image_size,
+                resize_to_max_side_len=self.resize_to_max_size_len,
+            ),
+            transforms.ToTensor(),
+            GlobalAndSplitImages(patch_size),
+        ])
+
+    def __call__(self, ls_ls_images: list[list[Image.Image]]) -> ImageBatchFeature:
+
+        processed_images = []
+        rows = []
+        cols = []
+
+        for ls_images in ls_ls_images:
+            row_images = []
+            assert len(
+                ls_images) == 1, "Currently only support 1 image per row for simplicity; this can be easily relaxed in the future if needed."
+            for image in ls_images:
+                image_tensor, (num_patches_h,
+                               num_patches_w) = self.transforms(image)
+                row_images.append(image_tensor)
+
+                rows.append([num_patches_h])
+                cols.append([num_patches_w])
+                processed_images.append(row_images)
+
+        processed_images = torch.tensor(processed_images)  # (bn, p, c, h, w)
+        rows = torch.tensor(rows)  # (bn, 1)
+        cols = torch.tensor(cols)  # (bn, 1)
+
+        bn, p, c, h, w = processed_images.shape
+
+        pixel_attention_mask = torch.ones((bn, p, h, w), dtype=torch.bool)
+
+        return ImageBatchFeature(
+            data={
+                "pixel_values": processed_images,
+                "pixel_attention_mask": pixel_attention_mask,
+                "rows": rows,
+                "cols": cols,
+            }
+        )
 
 
 class NanoVLMProcessor(ProcessorMixin):
     # TODO
-    ...
+    attributes = ["image_processor", "tokenizer"]
+
+    def __init__(
+        self,
+        image_processor,
+        tokenizer,
+        image_seq_len: int = 64,
+        **kwargs
+    ):
+        self.image_token = "<|image|>"
+        self.image_token_id = tokenizer.convert_tokens_to_ids(self.image_token)
+        self.global_image_token = "<|global_image|>"
+        self.global_image_token_id = tokenizer.convert_tokens_to_ids(
+            self.global_image_token)
+
+        self.image_processor = image_processor
+        self.tokenizer = tokenizer
+        self.image_seq_len = image_seq_len
+
+    def __call__(
+        self,
+        images: Union[Image.Image, list[Image.Image],
+                      list[list[Image.Image]]] = None,
+        text: Union[str, list[str]] = None,
+        audio=None,
+        videos=None,
+        image_seq_len: Optional[int] = None,
+        **kwargs,
+    ) -> BatchEncoding:
+        assert audio is None, "Audio input is not supported."
+        assert videos is None, "Video input is not supported."
+
+        image_seq_len = image_seq_len or self.image_seq_len
+
+        n_images_in_text = []
+        n_images_in_images = []
+        inputs = {}
+
+        if text is not None:
+            if isinstance(text, str):
+                text = [text]
+            if isinstance(text, list):
+                if not isinstance(text[0], str):
+                    raise ValueError(
+                        "All elements in the text list must be strings.")
+            n_images_in_text = [sample.count(
+                self.image_token) for sample in text]
+
+        ls_ls_images: list[list[Image.Image]] | None = None
+
+        if images is not None:
+            if isinstance(images, Image.Image):
+                ls_ls_images = [[images]]
+            elif isinstance(images, (list, tuple)) and isinstance(images[0], Image.Image):
+                images: list[Image.Image]
+                if text is not None:
+                    if sum(n_images_in_text) != len(images):
+                        raise ValueError(
+                            f"The total number of {self.image_token} tokens in the prompts should be the same as the number of images passed."
+                            f" Found {sum(n_images_in_text)} {self.image_token} tokens and {len(images)} images."
+                        )
+                    # Reorganize the images to match the prompts
+                    cumsum_images_in_text = [0] + \
+                        list(accumulate(n_images_in_text))
+                    ls_ls_images = [
+                        cast(list[Image.Image], images)[
+                            cumsum_images_in_text[i]: cumsum_images_in_text[i + 1]]
+                        for i in range(len(n_images_in_text))
+                    ]
+                else:
+                    ls_ls_images = [cast(list[Image.Image], images)]
+            elif (
+                not isinstance(images, (list, tuple))
+                and not isinstance(images[0], (list, tuple))
+                and not is_image_or_image_url(images[0][0])
+            ):
+                raise ValueError(
+                    "Invalid input images. Please provide a single image or a list of images or a list of list of images."
+                )
+            else:
+                ls_ls_images = cast(list[list[Image.Image]], images)
+
+            n_images_in_images = [len(imgs) for imgs in ls_ls_images]
+
+            image_inputs = self.image_processor(ls_ls_images)
+            inputs.update(image_inputs)
+
+            if text is not None:
+                if n_images_in_images != n_images_in_text:
+                    raise ValueError(
+                        "The number of images provided does not match the number of image tokens in the text prompts."
+                    )
+
+                image_rows = inputs.pop("rows", [[0] * len(text)])
+                image_cols = inputs.pop("cols", [[0] * len(text)])
+
+                image_token = self.image_token
+                global_img_token = self.global_image_token
+
+                prompt_strings = []
+                batch_image_seq_lengths = []
+
+                for sample, sample_rows, sample_cols in zip(text, image_rows, image_cols):
+                    image_prompt_strings = []
+                    image_seq_lengths = []
+
+                    for n_rows, n_cols in zip(sample_rows, sample_cols):
+                        image_prompt_string = ""
+                        count = 0
+                        for idx, (n_h, n_w) in enumerate(zip(n_rows, n_cols)):
+                            if len(n_rows) > 1:
+                                raise NotImplementedError(
+                                    "Multiple images per sample are not supported yet.")
+
+                            image_prompt_string += global_img_token
+                            count += 1
+                            image_prompt_string += image_token * image_seq_len
+                            count += image_seq_len
+
+                            if n_h == 1 and n_w == 1:
+                                continue
+
+                            for i in range(n_h):
+                                for j in range(n_w):
+                                    image_prompt_string += getattr(
+                                        self.tokenizer, f'r{i+1}c{j+1}')
+                                    image_prompt_string += image_token * image_seq_len
+                                    count += 1 + image_seq_len
+
+                        image_seq_lengths.append(count)
+                        image_prompt_strings.append(image_prompt_string)
+                    batch_image_seq_lengths.append(image_seq_lengths)
+                    split_image = sample.split(image_token)
+
+                    if len(split_image) == 0:
+                        raise ValueError(
+                            "The image token should be present in the text.")
+
+                    sample = split_image[0]
+                    for i, image_prompt_string in enumerate(image_prompt_strings):
+                        sample += str(image_prompt_string) + \
+                            str(split_image[i + 1])
+                    prompt_strings.append(sample)
+
+                text_inputs = self.tokenizer(
+                    prompt_strings
+                )
+
+                self._check_special_mm_tokens(
+                    prompt_strings,
+                    text_inputs,
+                    modalities=["image"]
+                )
+
+                inputs.update(text_inputs)
+
+        elif text is not None:
+            if any(n_images_in_text):
+                raise ValueError(
+                    f"Found {sum(n_images_in_text)} {self.image_token} tokens in the text but no images were passed."
+                )
+            text_inputs = self.tokenizer(text=text)
+            inputs.update(text_inputs)
+
+        return BatchFeature(data=inputs, tensor_type=None)
+
+    def _get_num_multimodal_tokens(self, image_sizes=None, **kwargs):
 
 
-class NanoVLMProcessingInfo(BaseProcessingInfo): 
+class NanoVLMProcessingInfo(BaseProcessingInfo):
     def get_hf_processor(self, **kwargs: object) -> NanoVLMProcessor:
-        ... 
-    
+        ...
+
+
 class NanoVLMDummyInputsBuilder(BaseDummyInputsBuilder[NanoVLMProcessingInfo]):
     ...
+
 
 class NanoVLMMultiModalProcessor(BaseMultiModalProcessor[NanoVLMProcessingInfo]):
     # TODO
@@ -71,14 +300,16 @@ class NanoVLMModel(nn.Module):
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
 
-        config: NanoVLMConfig = cast(NanoVLMConfig, vllm_config.model_config.hf_config)
+        config: NanoVLMConfig = cast(
+            NanoVLMConfig, vllm_config.model_config.hf_config)
 
         self.config = config
 
-        self.vision_encoder = ViT(config, prefix=maybe_prefix(prefix, "vision_encoder"))
+        self.vision_encoder = ViT(
+            config, prefix=maybe_prefix(prefix, "vision_encoder"))
 
         self.decoder = LanguageModel(
-            config, 
+            config,
             prefix=maybe_prefix(prefix, "decoder"),
         )
 
@@ -135,7 +366,7 @@ class NanoVLMModel(nn.Module):
     ) -> torch.Tensor | list[torch.Tensor]:
         if image_input["type"] == "image_embeds":
             return image_input["data"]
-        
+
         raise NotImplementedError()
 
         # TODO
@@ -152,7 +383,6 @@ class NanoVLMModel(nn.Module):
 
         return self._process_image_input(image_input)
 
-    
     def forward(
         self,
         input_ids: torch.Tensor | None,
@@ -160,8 +390,7 @@ class NanoVLMModel(nn.Module):
         intermediate_tensors: IntermediateTensors | None = None,
         inputs_embeds: torch.Tensor | None = None,
     ) -> torch.Tensor:
-       ...
-
+        ...
 
 
 @MULTIMODAL_REGISTRY.register_processor(
@@ -178,7 +407,7 @@ class NanoVLMForConditionalGeneration(nn.Module, SupportsMultiModal):
     def embed_multimodal(self, **kwargs: object) -> MultiModalEmbeddings:
         # TODO
         raise NotImplementedError()
-    
+
         image_input = self._parse_and_validate_image_input(**kwargs)
         if image_input is None:
             return []
@@ -195,10 +424,9 @@ class NanoVLMForConditionalGeneration(nn.Module, SupportsMultiModal):
     ) -> torch.Tensor | IntermediateTensors:
         # TODO
         raise NotImplementedError()
-    
+
     def compute_logits(self, hidden_states: torch.Tensor) -> torch.Tensor:
         # TODO
         raise NotImplementedError()
         logits = self.logits_processor(self.lm_head, hidden_states)
         return logits
-
